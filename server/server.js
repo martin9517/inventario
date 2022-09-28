6779ce758f155cb8bca09a1e8e660e19bc6e9796
patobressan <patriciobressan@gmail.com>
2022-09-28 18:36:54 -0300

    change port

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ require("./app/routes/bonificacion.routes")(app);
 require("./app/routes/productoDetalle.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
